ca45f03b091d7f007f2788eed4e9def4701cba4c
deva5512 <devendran5512@gmail.com>
2026-02-28 19:13:49 +0530

    updated CORS Origin

--- main.py
+++ main.py
@@ -17,17 +17,10 @@ app = FastAPI(
     version="1.0.0",
 )
 
-# CORS Middleware
-origins = [
-    "http://localhost",
-    "http://localhost:5173",
-    "https://tech-endeavor.vercel.app/",
-    "https://tech-endeavor-react.vercel.app",
-]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
