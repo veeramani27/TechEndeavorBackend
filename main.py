0c3496298bb3f1252776a8bf32e1159bbdd98d6f
veeramani27 <veeramani27112004@gmail.com>
2026-06-13 23:27:36 +0530

    cors update

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:3000"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:3000", "https://tech-endeavor.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
